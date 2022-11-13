c12fabedc0f6b990c9c78d5ee21f0ead1a6ba60d
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2022-11-13 20:16:35 -0300

    refactor: remove home css

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,8 @@
-import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Joga+ UFCG</title>
-        <meta
-          name="description"
-          content="Todas as quadras da UFCG disponíveis para agendamento fácil pelo site"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <h1>Home de login</h1>
     </div>
   );
