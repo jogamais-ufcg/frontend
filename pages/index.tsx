04f7979381a051ac9145452ac8ab5838bae2925e
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2022-11-13 19:32:26 -0300

    feat: add next folder to eslint ignore

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,19 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
         <title>Quadras - Joga+ UFCG</title>
-        <meta name="description" content="Todas as quadras da UFCG disponíveis para agendamento fácil pelo site" />
+        <meta
+          name="description"
+          content="Todas as quadras da UFCG disponíveis para agendamento fácil pelo site"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <h1>Listagem de quadras</h1>
     </div>
-  )
+  );
 }
