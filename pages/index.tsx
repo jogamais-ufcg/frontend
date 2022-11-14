f9f1f077c932b66c5bf93f44d09f31ad3681d05b
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2022-11-13 21:13:05 -0300

    feat: add logo and titles to home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,26 @@
+import Image from 'next/image';
 import styles from '../styles/Home.module.css';
+import icon from '../public/brand/icon.png';
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1>Home de login</h1>
+      <div className={styles.logoContainer}>
+        <Image className={styles.logo} src={icon} alt="Joga Mais UFCG" />
+      </div>
+
+      <div className={styles.mainInformation}>
+        <h1>Agende com tranquilidade o seu horário</h1>
+        <p>
+          Todas as quadras da UFCG estão disponíveis para agendamento fácil pelo
+          site, acesse ou crie a sua conta!
+        </p>
+      </div>
+
+      <div>
+        <button type="button">Dar uma olhadinha nas quadras</button>
+        <button type="button">Realizar login</button>
+      </div>
     </div>
   );
 }
