c43517c23505c39999bb0d113fad92fa6884b37b
luisricarte <luis.ricarte.ext@synchro.com.br>
2022-11-16 20:56:23 -0300

    chore: lint

--- pages/redefinir-senha/index.tsx
+++ pages/redefinir-senha/index.tsx
@@ -5,10 +5,7 @@ import styles from './styles.module.css';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 import logo from '../../public/brand/logo.png';
-import {
-  faLock,
-  faSignIn,
-} from '@fortawesome/free-solid-svg-icons';
+import { faLock, faSignIn } from '@fortawesome/free-solid-svg-icons';
 
 export default function RedefinirSenha() {
   const router = useRouter();
@@ -27,13 +24,12 @@ export default function RedefinirSenha() {
       </div>
 
       <div className={styles.inputsContainer}>
-
-          <Input
-            icon={faLock}
-            label="Nova senha"
-            placeholder="Digite aqui sua senha"
-            type="password"
-          />
+        <Input
+          icon={faLock}
+          label="Nova senha"
+          placeholder="Digite aqui sua senha"
+          type="password"
+        />
 
         <div className={styles.input}>
           <Input
