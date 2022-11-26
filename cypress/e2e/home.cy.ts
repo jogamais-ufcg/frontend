34a34211536d5f23eefb03d722153e063d79c7ad
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2022-11-26 16:13:41 -0300

    fix: add global scope to cypress

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -2,6 +2,7 @@ describe('home page', () => {
   it('exists main title', () => {
     cy.visit('/');
     cy.get('h1').should('contain', 'Agende com tranquilidade o seu horário');
-    
   });
 });
+
+export {};
