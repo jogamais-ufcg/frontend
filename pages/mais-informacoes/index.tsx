8079ce0b7ae0428e09a8277b8a438c918f7229f1
Luis Ricarte <luis.ricarte@ccc.ufcg.edu.br>
2022-12-02 11:28:22 -0300

    fix: remove imports

--- pages/mais-informacoes/index.tsx
+++ pages/mais-informacoes/index.tsx
@@ -1,10 +1,7 @@
-import { faEnvelope, faLock, faCheck } from '@fortawesome/free-solid-svg-icons';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
 import Input from '../../components/Input';
 import styles from './styles.module.css';
-import Image from 'next/image';
-import Link from 'next/link';
 import { useRouter } from 'next/router';
-import logo from '../../public/brand/logo.png';
 import Button from '../../components/Button';
 import PageContainer from '../../components/PageContainer';
 import BackHeader from 'components/BackHeader';
@@ -15,7 +12,7 @@ export default function Login() {
   return (
     <PageContainer headTitle="Login">
       <BackHeader title='Mais informações'></BackHeader>
-        
+
       <Input 
       label="Lista de Jogadores"
       placeholder='Preencher com espaços separados por vírgulas ou adicionando novas linhas.'
