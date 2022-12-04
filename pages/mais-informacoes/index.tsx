bd698083f67b9b264a86e9f9a0e3c351f9a54a33
kmmelcher <kilian.melcher@gmail.com>
2022-12-03 23:24:00 -0300

    feat: tela mais informações agendamentos (25)

--- pages/mais-informacoes/index.tsx
+++ pages/mais-informacoes/index.tsx
@@ -1,4 +1,9 @@
-import { faCheck } from '@fortawesome/free-solid-svg-icons';
+import {
+  faCheck,
+  faEnvelope,
+  faCircleUser,
+  faPhone,
+} from '@fortawesome/free-solid-svg-icons';
 import Input from '../../components/Input';
 import styles from './styles.module.css';
 import { useRouter } from 'next/router';
@@ -13,11 +18,35 @@ export default function Login() {
     <PageContainer headTitle="Login">
       <BackHeader title="Mais informações"></BackHeader>
 
-      <Input
-        label="Lista de Jogadores"
-        placeholder="Preencher com espaços separados por vírgulas ou adicionando novas linhas."
-        type="textarea"
-      />
+      <div className={styles.inputsContainer}>
+        <Input
+          icon={faEnvelope}
+          label="Email"
+          placeholder="meumelhor@email.com"
+          type="email"
+        />
+
+        <Input
+          icon={faCircleUser}
+          label="Nome Completo"
+          placeholder="Nome Completo"
+          type="text"
+        />
+
+        <Input
+          icon={faPhone}
+          label="Número do Whatsapp"
+          mask="(99) 9 9999-9999"
+          placeholder="(99) 9 4002-8922"
+          type="tel"
+        />
+
+        <Input
+          label="Lista de Jogadores"
+          placeholder="Preencher com espaços separados por vírgulas ou adicionando novas linhas."
+          type="textarea"
+        />
+      </div>
 
       <div className={styles.buttonContainer}>
         <Button
