7315b31522d854209110a1276c011d4d6bedf109
francisco <lucas.araripina@hotmail.com>
2022-12-05 23:53:33 -0300

    feat/ registered-successfully screen

---
+++ pages/gerenciar-usuarios/cadastro-sucesso/index.tsx
@@ -0,0 +1,39 @@
+import Image from 'next/image';
+// import { useRouter } from 'next/router';
+import logo from '../../../public/brand/logo.png';
+import hand from '../../../public/illustrations/hand.svg';
+import styles from './styles.module.css';
+import Button from '../../../components/Button';
+import PageContainer from '../../../components/PageContainer';
+
+export default function SignInConfirmation() {
+  // const router = useRouter();
+
+  return (
+    <PageContainer headTitle="Cadastro confirmado">
+      <div className={styles.logoContainer}>
+        <Image className={styles.logo} src={logo} alt="Joga Mais UFCG" />
+      </div>
+      <div className={styles.mainInformation}>
+        <h1>Cadastro realizado com sucesso!</h1>
+        <p>A sua resposta foi enviada por email para o usuário solicitante.</p>
+      </div>
+
+      <div className={styles.handContainer}>
+        <Image
+          className={styles.hand}
+          src={hand}
+          alt="Mão Segurando Telefone"
+        />
+      </div>
+
+      <div className={styles.buttonsContainer}>
+        <Button
+          type="button"
+          label="👀 Voltar à listagem de usuários"
+          color="secondary"
+        />
+      </div>
+    </PageContainer>
+  );
+}
