088a2b7d5e2bf51ef0f8e2a7027ce9925f913653
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2022-12-09 19:24:49 -0300

    feat: add initial css page

---
+++ pages/quadras/data.ts
@@ -0,0 +1,38 @@
+export const mockedCourts = [
+  {
+    id: 1,
+    name: 'Quadra de Tênis',
+    description:
+      'Lorem Impsum Dolor Sit Amet Lorem Impsum Dolor Sit Amet Lorem Impsum Dolor Sit Amet Lorem Impsum Dolor Sit Amet Lorem Impsum Dolor Sit Amet Lorem Impsum Dolor Sit Amet',
+  },
+  {
+    id: 2,
+    name: 'Quadra de Futebol',
+    description:
+      'Lorem Impsum Dolor Sit Amet Lorem Impsum Dolor Sit Amet Lorem Impsum Dolor Sit Amet Lorem Impsum Dolor Sit Amet Lorem Impsum Dolor Sit Amet Lorem Impsum Dolor Sit Amet',
+  },
+  {
+    id: 3,
+    name: 'Quadra de Vôlei',
+    description:
+      'Lorem Impsum Dolor Sit Amet Lorem Impsum Dolor Sit Amet Lorem Impsum Dolor Sit Amet Lorem Impsum Dolor Sit Amet Lorem Impsum Dolor Sit Amet Lorem Impsum Dolor Sit Amet',
+  },
+  {
+    id: 4,
+    name: 'Quadra de Basquete',
+    description:
+      'Lorem Impsum Dolor Sit Amet Lorem Impsum Dolor Sit Amet Lorem Impsum Dolor Sit Amet Lorem Impsum Dolor Sit Amet Lorem Impsum Dolor Sit Amet Lorem Impsum Dolor Sit Amet',
+  },
+  {
+    id: 5,
+    name: 'Quadra de Futebol Society',
+    description:
+      'Lorem Impsum Dolor Sit Amet Lorem Impsum Dolor Sit Amet Lorem Impsum Dolor Sit Amet Lorem Impsum Dolor Sit Amet Lorem Impsum Dolor Sit Amet Lorem Impsum Dolor Sit Amet',
+  },
+  {
+    id: 6,
+    name: 'Quadra de Handebol',
+    description:
+      'Lorem Impsum Dolor Sit Amet Lorem Impsum Dolor Sit Amet Lorem Impsum Dolor Sit Amet Lorem Impsum Dolor Sit Amet Lorem Impsum Dolor Sit Amet Lorem Impsum Dolor Sit Amet',
+  },
+];
---
+++ pages/quadras/index.tsx
@@ -0,0 +1,77 @@
+import Image from 'next/image';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faSignIn,
+  faBars,
+  faPlus,
+  faSearch,
+} from '@fortawesome/free-solid-svg-icons';
+import PageContainer from 'components/PageContainer';
+import logo from 'public/brand/logo.png';
+import styles from './styles.module.css';
+import { useState } from 'react';
+import Input from 'components/Input';
+
+interface OptionsMenuProps {
+  isLogged: boolean;
+  isAdmin: boolean;
+}
+
+function OptionsMenu({ isLogged, isAdmin }: OptionsMenuProps) {
+  if (isLogged) {
+    if (isAdmin) {
+      return (
+        <>
+          <FontAwesomeIcon icon={faBars} />
+          <h3>Opções</h3>
+        </>
+      );
+    }
+
+    return (
+      <>
+        <FontAwesomeIcon icon={faBars} />
+        <h3>Olá, Davi!</h3>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <FontAwesomeIcon icon={faSignIn} />
+      <h3>Realizar login</h3>
+    </>
+  );
+}
+
+export default function Courts() {
+  const [isAdmin] = useState(true);
+
+  return (
+    <div>
+      <header className={styles.header}>
+        <Image src={logo} alt="Joga Mais UFCG" width={100} />
+
+        <button>
+          <OptionsMenu isAdmin={isAdmin} isLogged={true} />
+        </button>
+      </header>
+
+      <PageContainer headTitle="Quadras disponíveis">
+        <aside className={styles.title}>
+          <h1>Quadras disponíveis</h1>
+
+          {isAdmin && (
+            <button>
+              <FontAwesomeIcon icon={faPlus} />
+            </button>
+          )}
+        </aside>
+
+        <Input type="text" placeholder="Pesquise por nome..." icon={faSearch} />
+
+        <div className={styles.courtsContainer}></div>
+      </PageContainer>
+    </div>
+  );
+}
