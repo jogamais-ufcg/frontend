e755eb69bdc43df2349984d391bfc49b0fb16d20
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2022-12-09 16:35:05 -0300

    feat: add subtitle to back header component

---
+++ pages/agendamentos-admin/index.tsx
@@ -0,0 +1,10 @@
+import BackHeader from 'components/BackHeader';
+import PageContainer from 'components/PageContainer';
+
+export default function AdminSchedules() {
+  return (
+    <PageContainer headTitle="Agendamentos">
+      <BackHeader title="Agendamentos" subtitle="19/02/2023" />
+    </PageContainer>
+  );
+}
