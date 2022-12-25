b8d1a079f5d6648a2b62d069b453402d1d2136e1
Kilian Melcher <kilian.melcher@gmail.com>
2022-12-25 11:41:35 -0300

    fix: Add margin to modal component

--- components/Modal/index.tsx
+++ components/Modal/index.tsx
@@ -38,43 +38,44 @@ function Modal({ open, onOpen, onClose }: ModalProps) {
       onClose={onClose}
     >
       <Box className={styles.modalBox}>
-        <List
-          className={styles.list}
-          subheader={
-            <div className={styles.subheader}>
-              <AccountBoxIcon />
-              <h2>Meu perfil</h2>
-              <div className={styles.close} onClick={onClose}>
-                <CloseIcon />
+        <div className={styles.list}>
+          <List
+            subheader={
+              <div className={styles.subheader}>
+                <AccountBoxIcon />
+                <h2>Meu perfil</h2>
+                <div className={styles.close} onClick={onClose}>
+                  <CloseIcon />
+                </div>
               </div>
-            </div>
-          }
-        >
-          <ListItem key="edit-profile" className={styles.listItem}>
-            <ListItemButton>
-              <ListItemIcon>
-                <EditIcon fontSize="large" />
-              </ListItemIcon>
-              <p>Editar perfil</p>
-            </ListItemButton>
-          </ListItem>
-          <ListItem key="my-appointments" className={styles.listItem}>
-            <ListItemButton>
-              <ListItemIcon>
-                <HistoryIcon fontSize="large" />
-              </ListItemIcon>
-              <p>Meus agendamentos</p>
-            </ListItemButton>
-          </ListItem>
-          <ListItem key="logout" className={styles.listItem}>
-            <ListItemButton>
-              <ListItemIcon>
-                <LogoutIcon fontSize="large" />
-              </ListItemIcon>
-              <p>Sair da conta</p>
-            </ListItemButton>
-          </ListItem>
-        </List>
+            }
+          >
+            <ListItem key="edit-profile" className={styles.listItem}>
+              <ListItemButton>
+                <ListItemIcon>
+                  <EditIcon fontSize="large" />
+                </ListItemIcon>
+                <p>Editar perfil</p>
+              </ListItemButton>
+            </ListItem>
+            <ListItem key="my-appointments" className={styles.listItem}>
+              <ListItemButton>
+                <ListItemIcon>
+                  <HistoryIcon fontSize="large" />
+                </ListItemIcon>
+                <p>Meus agendamentos</p>
+              </ListItemButton>
+            </ListItem>
+            <ListItem key="logout" className={styles.listItem}>
+              <ListItemButton>
+                <ListItemIcon>
+                  <LogoutIcon fontSize="large" />
+                </ListItemIcon>
+                <p>Sair da conta</p>
+              </ListItemButton>
+            </ListItem>
+          </List>
+        </div>
       </Box>
     </SwipeableDrawer>
   );
