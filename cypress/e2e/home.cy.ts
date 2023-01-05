412971e31e53a2d0d50477b7ea988bd11d988079
kmmelcher <kilian.melcher@gmail.com>
2023-01-05 16:10:31 -0300

    Add first test

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,7 +1,13 @@
-describe('home page', () => {
-  it('exists main title', () => {
+describe('Cadastrar usuário', () => {
+  it('Sucessful register', () => {
     cy.visit('/');
-    cy.get('h1').should('contain', 'Agende com tranquilidade o seu horário');
+    cy.get('a').contains('Cadastre-se').click();
+    cy.get("input[type='email']").type('kilian.melcher@gmail.com');
+    cy.get("input[type='text']").type('Kilian Macedo Melcher');
+    cy.get("input[type='tel']").type('83986650905');
+    cy.get("input[type='password']:first").type('senhasegura123');
+    cy.get("input[type='password']:last").type('senhasegura123');
+    cy.get("button").contains('Próxima etapa').click();
   });
 });
 
